cffcc16ee2f4763d83b17c66442a81628bcaaa99
User Joseph D Github <joseph.ramli@binus.ac.id>
2018-10-16 09:43:19 +0700

    Project Added
    Project Added

---
+++ Assets/script/WaterTapButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTapButton : MonoBehaviour {
+
+    public GameObject water;
+    public GameObject canvasA;
+    public GameObject canvasB;
+    private GameObject spawnedEffect;
+    private ParticleSystem ps;
+
+
+    private bool isOn = false;
+
+
+	public void tap()
+    {
+        if (!isOn) {
+            spawnedEffect = (GameObject)Instantiate(water, water.transform.position, water.transform.rotation);
+            isOn = true;
+        }
+        else
+        {
+            
+
+            ps = water.GetComponent<ParticleSystem>();
+            var main = ps.main;
+            main.loop = false;
+
+            Invoke("waitstuff", 2);
+
+            
+        }
+    }
+
+    void waitstuff()
+    {
+        Destroy(spawnedEffect);
+        isOn = false;
+        canvasA.SetActive(false);
+        canvasB.SetActive(true);
+    }
+    
+}
---
+++ Assets/script/touchtest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchtest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount>0)
+		{
+			foreach(Touch touch in Input.touches)
+			{
+				Debug.Log(touch.position);
+			}
+		}
+	}
+}
